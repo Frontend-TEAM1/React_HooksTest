478c15186fecbb26b5a281c18a154d312a27be69
JuramLee <113501460+JuramLee@users.noreply.github.com>
2023-02-19 23:46:24 +0900

    Update product.js

--- src/components/2.state/product.js
+++ src/components/2.state/product.js
@@ -1,21 +1,21 @@
 import styled from "styled-components";
 
-function ProductCard({ onNavigate, product }) {
-
+function ProductCard({ detail, onNavigate }) {
+  const handleClick = () => {
+    onNavigate(detail.productNumber);
+  };
+  console.log("=====>" + detail.productNumber);
   return (
-    <>
-      <S.Item onClick={() => onNavigate(product.productNumber)}>
-        <h4>{product.productName}</h4>
-        <p>{"상품번호: " + product.productNumber}</p>
-        <p>
-          {"가격: " +
-            product.productPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-        </p>
-        <p>{"사이즈: " + product.productSize}</p>
-        <p>{"평점: " + product.productRating}</p>
-        <p>{"리뷰: " + product.productReview}</p>
-      </S.Item>
-    </>
+    <S.Item onClick={handleClick}>
+      {/*<S.Item onClick={handleClick}>*/}
+      <h4>{detail.productName}</h4>
+      <p>상품번호: {detail.productNumber}</p>
+      <p>가격: {Number(detail.productPrice).toLocaleString()}원</p>
+      {/*toLocalString()으로 되는 방법 찾아보기*/}
+      <p>사이즈: {detail.productSize}</p>
+      <p>평점: {detail.productRating}</p>
+      <p>리뷰: {detail.productReview}</p>
+    </S.Item>
   );
 }
 export default ProductCard;
